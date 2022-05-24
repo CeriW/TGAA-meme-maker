5e6165eb4e6e137a1934516a24ce4edc1da422e2
CeriW <cxv712@gmail.com>
2022-05-24 19:45:19 +0100

    Character selector basics
    You can now choose characters - at present only Sholmes and Iris, and a single pose for each.

--- meme-generator.js
+++ meme-generator.js
@@ -4,32 +4,37 @@ let backgroundImg = document.querySelector('#background')
 let backgroundSelector = document.querySelector('#background-selector')
 
 let characterImg = document.querySelector('#character')
+let characterSelector = document.querySelector('#character-selector')
 
-//let locationBtn = document.querySelector('#change-location')
-//locationBtn.addEventListener('click', changeLocation)
 
 generatePanel()
 backgroundSelector.addEventListener('change', generatePanel)
+characterSelector.addEventListener('change', generatePanel)
+
 
 function generatePanel(){
-  //alert('hi!')
   backgroundImg.setAttribute('src', backgroundSelector.value)
+  characterImg.setAttribute('src', characterSelector.value)
+  
   
   // Wait for the image to load before we attempt to add it to the canvas
   backgroundImg.addEventListener('load', function(){
-      var c = document.getElementById("myCanvas");
-      var ctx = c.getContext("2d");
-      var img = document.querySelector("#background");
-      ctx.drawImage(img, 0, 0);
+    addImage('background')
   })
+
+  character.addEventListener('load', function(){
+    addImage('character')
+  })
+
   
-    // Wait for the image to load before we attempt to add it to the canvas
-  backgroundImg.addEventListener('load', function(){
-      var c = document.getElementById("myCanvas");
-      var ctx = c.getContext("2d");
-      var img = document.querySelector("#character");
-      ctx.drawImage(img, 0, 0);
-  })
+  // Adds a new layer to the canvas.
+  //Acceptable parameter values: background, character
+  function addImage(type){
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    var img = document.querySelector("#" + type);
+    ctx.drawImage(img, 0, 0);
+  }
 
 }
 
