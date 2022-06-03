af5649cdcfc6864d229bf56d4689333031092be4
CeriW <cxv712@gmail.com>
2022-06-03 21:30:01 +0100

    Location rearrangement
    Rearranged the location list in a rough order of importance.

--- meme-generator.js
+++ meme-generator.js
@@ -37,18 +37,18 @@ let paths ={
 // id - the name of the image to go alongside it
 const locations = [
   {name: "221B Baker Street",                     id:"baker-street-221b"},
-  {name: "221B Baker Street",                     id:"baker-street-221b-night", variant:"night"},
-  {name:"Crystal Tower base",                     id:"crystal-tower-base"},
+  {name: "221B Baker Street",                     id:"baker-street-221b-night",                         variant:"night"},
+  {name: "Sholmes' Suite",                        id:"sholmes-suite",                                   variant: "Fireplace"},
+  {name: "Sholmes' Suite",                        id:"sholmes-suite-iris",                              variant:"Iris' side"},
+  {name: "Sholmes' Suite",                        id:"sholmes-suite-sholmes",                           variant:"Herlock's side"},
+  {name: "Crystal Tower base",                    id:"crystal-tower-base"},
   {name: "The Garridebs' room",                   id:"garridebs-room"},
   {name: "Naruhodo's Legal Consultancy",          id:"naruhodos-legal-consultancy"},
-  {name: "Sholmes' Suite",                        id:"sholmes-suite", variant: "Fireplace"},
-  {name: "Defendant's Antechamber", id:"defendants-antechamber-the-old-bailey-left", variant:'The Old Bailey'},
-  {name: "Natsume's Room", id:"natsumes-room"},
-  {name: "Prison cell", id:"prison-cell"},
-  {name: "Prosecutor's Office", id:"prosecutors-office-left"},
-  {name: "Sholmes' Suite", id:"sholmes-suite-iris", variant:"Iris' side"},
-  {name: "Sholmes' Suite", id:"sholmes-suite-sholmes"},
-  {name: "Windibank's Pawnbrokery", id:"windibanks"},
+  {name: "Defendant's Antechamber",               id:"defendants-antechamber-the-old-bailey-left",      variant:'The Old Bailey'},
+  {name: "Natsume's Room",                        id:"natsumes-room"},
+  {name: "Prison cell",                           id:"prison-cell"},
+  {name: "Prosecutor's Office",                   id:"prosecutors-office-left"},
+  {name: "Windibank's Pawnbrokery",               id:"windibanks"},
 ]
 
 // A list of the available characters
