a156b83c501a88f1a528eaad091708f9691f8134
CeriW <cxv712@gmail.com>
2023-01-13 15:40:55 +0000

    Update weather bar styling

--- meme-generator.js
+++ meme-generator.js
@@ -500,7 +500,7 @@ function download() {
   let formattedDate = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " +  date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
 
 
-  downloadButton.download = `tgaa-meme-maker-${formattedDate}.png`;
+  downloadButton.download = `tgaa-meme-maker - ${formattedDate}.png`;
   downloadButton.href = downloadableCanvas.toDataURL();
   downloadableCanvas.remove();
 }
