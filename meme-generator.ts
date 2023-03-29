835cf8536308188ef3a02ee240fe81ac7b3f21d2
Ceri <ceriwoolway@gmail.com>
2023-03-29 21:56:28 +0100

    Update version number to force rebuild

--- meme-generator.ts
+++ meme-generator.ts
@@ -1,5 +1,5 @@
 // Version info
-const versionInfo = '4.2.0 - 2023-03-29';
+const versionInfo = '4.2.0 - 2023-03-29 d';
 
 import { locations, LocationObject } from "./locations.js";
 import { characters, CharacterObject } from "./characters.js";
