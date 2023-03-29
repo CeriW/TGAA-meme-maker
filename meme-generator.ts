56e21e47b5f73820104574dd7bfdc819dce06440
Ceri <ceriwoolway@gmail.com>
2023-03-29 17:08:53 +0100

    Update version numbers

--- meme-generator.ts
+++ meme-generator.ts
@@ -1,5 +1,5 @@
 // Version info
-const versionInfo = '4.1.0 - 2023-03-24';
+const versionInfo = '4.2.0 - 2023-03-29';
 
 import { locations, LocationObject } from "./locations.js";
 import { characters, CharacterObject } from "./characters.js";
