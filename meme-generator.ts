57dfd922c3272733ee7ee24881c9646789673fc0
Ceri <ceriwoolway@gmail.com>
2023-04-12 14:57:46 +0100

    Bugfix 9

--- meme-generator.ts
+++ meme-generator.ts
@@ -775,7 +775,7 @@ function download(e: Event) {
         if (imgToDraw) {
           imgToDraw.width = 1920;
           imgToDraw.height = 1080;
-          tempCanvasContext?.drawImage(imgToDraw, 0, 0, 1920, 1080);
+          tempCanvasContext.drawImage(imgToDraw, 0, 0, 1920, 1080);
         }
       }
 
